cd4c96d5a7c8e8c960a87e834ae3f40df8547148
Nicolas Useche <nico311021@gmail.com>
2023-06-02 01:38:57 -0500

    Fix bugs

--- App/models.py
+++ App/models.py
@@ -1,31 +1,31 @@
-
 from datetime import datetime
 from . import db
 from flask_login import UserMixin
+from sqlalchemy import LargeBinary
 
 
 class User(db.Model, UserMixin):
-
-    id = db.Column(db.Integer, primary_key = True)
-    first_name = db.Column(db.String(255), nullable = False)
-    second_name = db.Column(db.String(255), nullable = False)
-    email = db.Column(db.String(255), unique = True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(255), nullable=False)
+    second_name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
-    galleries = db.relationship('Gallery')
+    galleries = db.relationship("Gallery")
 
 
 class Gallery(db.Model):
-
-    id = db.Column(db.Integer, primary_key = True)
-    gallery_name = db.Column(db.String(255), nullable = False)
-    date = db.Column(db.DateTime,  default = datetime.utcnow())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    images = db.relationship('Image')
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    gallery_name = db.Column(db.String(255), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow())
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    images = db.relationship("Image")
 
 
 class Image(db.Model):
-
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     filename = db.Column(db.String(255))
-    gallery_id = db.Column(db.Integer, db.ForeignKey('gallery.id'))
-
+    gallery_id = db.Column(db.Integer, db.ForeignKey("gallery.id"))
+    data = db.Column(
+        LargeBinary
+    )  # Change 'filename' to 'data' and the type to LargeBinary
+    mime_type = db.Column(db.String(64))
